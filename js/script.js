a6be827c608fdfefa8317d1816b946c2d0ef1454
Semen Lebedew <mamontedinorog@yandex.by>
2023-06-20 12:00:02 +0300

    scroll to top

--- js/script.js
+++ js/script.js
@@ -197,3 +197,35 @@ function burderShow() {
   burgerPopup.classList.toggle('show_burger')
   document.querySelector('body').classList.toggle('noscroll')
 }
+
+//кнопка наверх
+const goTopBtn = document.querySelector(".block__up");
+
+// обработчик на скролл окна
+window.addEventListener("scroll", trackScroll);
+// обработчик на нажатии
+goTopBtn.addEventListener("click", goTop);
+
+function trackScroll() {
+  // вычисляем положение от верхушки страницы
+  const scrolled = window.pageYOffset;
+  // считаем высоту окна браузера
+  const coords = document.documentElement.clientHeight;
+  // если вышли за пределы первого окна
+  if (scrolled > coords) {
+    // кнопка появляется
+    goTopBtn.classList.add("go-top--show");
+  } else {
+    // иначе исчезает
+    goTopBtn.classList.remove("go-top--show");
+  }
+}
+
+function goTop() {
+  // пока не вернулись в начало страницы
+  if (window.pageYOffset > 0) {
+    // скроллим наверх
+    window.scrollBy(0, -75); // второй аргумент - скорость
+    setTimeout(goTop, 0); // входим в рекурсию
+  }
+}
